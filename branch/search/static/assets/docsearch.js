2663e40c475607bef9400de5cb5ae80282193fea
CircleCI <circleci@transferwise.com>
2021-04-26 12:03:58 +0000

    Updates

--- branch/search/static/assets/docsearch.js
+++ branch/search/static/assets/docsearch.js
@@ -1,7 +1,14 @@
+/**
+ * https://docsearch.algolia.com/docs/behavior
+ */
 // eslint-disable-next-line no-undef
 docsearch({
   apiKey: 'a7106c2ce36353c2046f1d48a42be7bc',
   indexName: 'transferwise',
   inputSelector: '#the-search',
   debug: true,
+  transformData(args) {
+    console.log('in transformData', args);
+    return args;
+  },
 });
