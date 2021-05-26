9e263594212f5cc22cca06f7515ad15c84bd2a46
Richard Bata <richard.bata@transferwise.com>
2021-05-26 23:26:04 +0200

    Making the error boundary text translatable

---
+++ packages/dynamic-flows/src/flow/errorBoundary/ErrorBoundary.messages.js
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  errorAlert: {
+    id: 'dynamicFlows.ErrorBoundary.errorAlert',
+    defaultMessage: 'Oops. Something went wrong...',
+    description: 'Description of Dynamic flows error alert',
+  },
+});
