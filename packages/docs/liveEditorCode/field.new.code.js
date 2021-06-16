e244df77618aa364dabf731a9f3e47b1f2d005c4
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-06-16 10:32:04 +0100

    docs: updated docs

---
+++ packages/docs/liveEditorCode/field.new.code.js
@@ -0,0 +1,34 @@
+() => {
+  const [value, setValue] = React.useState('');
+  const [validation, setValidation] = React.useState([]);
+
+  const handleOnChange = (val) => {
+    const validations = {
+      minLength: { value: 3, message: 'Insert a value longer than 3' },
+      maxLength: { value: 6, message: 'Insert a value shorter than 6' },
+    };
+
+    const failures = getFieldValidationFailures({
+      value: val,
+      validations,
+      isRequired: true,
+      type: 'string',
+    });
+
+    setValidation(failures);
+    setValue(val);
+  };
+
+  return (
+    <Field
+      label="Text Field"
+      messages={{
+        help: 'help message',
+        error: 'manual error',
+        validation,
+      }}
+    >
+      <input type="text" value={value} onChange={(val) => handleOnChange(val)} />
+    </Field>
+  );
+};
