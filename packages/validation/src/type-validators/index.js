db76e26cae5c805ef6d2ce9e3c95bf5ca8173368
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-06-16 17:21:53 +0100

    Update index.js

--- packages/validation/src/type-validators/index.js
+++ packages/validation/src/type-validators/index.js
@@ -3,8 +3,6 @@ const isNumber = (value) => typeof value === 'number' && !Number.isNaN(value);
 const isInteger = (value) => {
   return isNumber(value) && Math.floor(value) === value;
 };
-
-const isDate = (date) => validDateObject(date) || validDateString(date);
 const isBoolean = (value) => typeof value === 'boolean';
 
 const isObject = (value) => !isNull(value) && !isUndefined(value) && value.constructor === Object;
@@ -12,9 +10,4 @@ const isArray = (value) => Array.isArray(value);
 const isNull = (value) => value === null;
 const isUndefined = (value) => typeof value === 'undefined';
 
-const validDateString = (dateString) =>
-  typeof dateString === 'string' && validDateObject(new Date(dateString));
-
-const validDateObject = (dateObj) => dateObj instanceof Date && !isNaN(dateObj); // eslint-disable-line no-restricted-globals
-
-export { isString, isNumber, isInteger, isDate, isBoolean, isObject, isArray, isNull, isUndefined };
+export { isString, isNumber, isInteger, isBoolean, isObject, isArray, isNull, isUndefined };
