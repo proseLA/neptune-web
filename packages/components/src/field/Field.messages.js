e850a2450978035d7152693dbbd71325c8115302
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-06-16 17:02:55 +0100

    chore: change translation type

--- packages/components/src/field/Field.messages.js
+++ packages/components/src/field/Field.messages.js
@@ -1,8 +1,6 @@
 import { defineMessages } from 'react-intl';
 
-let messages: {
-  [index: string]: { id: string; defaultMessage: string; description: string };
-} = defineMessages({
+const messages = defineMessages({
   minLength: {
     id: 'minLength',
     defaultMessage: 'Please type a value longer than {minLength}',
