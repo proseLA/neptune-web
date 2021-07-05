1d84e54590384677369403b83b771f9be0fa46db
Julian Garamendy <67635620+JulianG-TW@users.noreply.github.com>
2021-07-05 15:39:13 +0100

    Improve PromotedOneOfSchema default selection (#1036)
    * Extract OneOfSchemaModelMatcher
    
    * Improve default selection in PromotedOneOfSchema
    
    We found and edge case in PromotedOneOfSchema, when selecting the default schema.
    
    It can be fixed by applying the same logic we use in OneOfSchema.

--- packages/dynamic-flows/src/jsonSchemaForm/oneOfSchema/OneOfSchema.js
+++ packages/dynamic-flows/src/jsonSchemaForm/oneOfSchema/OneOfSchema.js
@@ -13,14 +13,11 @@ import { isValidSchema } from '../../common/validation/schema-validators';
 
 import DynamicAlert from '../../layout/alert';
 import Help from '../help';
-
-function isConstSchema(schema) {
-  return !!schema && (schema.const || (schema.enum && schema.enum.length === 1));
-}
-
-function isNoNConstSchema(schema) {
-  return !!schema && !isConstSchema(schema);
-}
+import {
+  getBestMatchingSchemaIndexForModel,
+  isConstSchema,
+  isNoNConstSchema,
+} from './OneOfSchemaModelMatcher';
 
 const OneOfSchema = (props) => {
   const [changed, setChanged] = useState(false);
@@ -43,31 +40,6 @@ const OneOfSchema = (props) => {
 
   const getValidIndexFromDefault = (schema) =>
     schema.oneOf.findIndex((childSchema) => isValidSchema(schema.default, childSchema));
-
-  const getSchemaProperties = (schema) =>
-    schema.properties !== null && typeof schema.properties === 'object'
-      ? Object.entries(schema.properties)
-      : [];
-
-  const getBestMatchingSchemaIndexForModel = (schema, model) => {
-    const schemaPoints = schema.oneOf.map((childSchema) => {
-      let points = 0;
-      getSchemaProperties(childSchema).forEach(([key, propertySchema]) => {
-        if (isConstSchema(propertySchema) && propertySchema.const === model[key]) {
-          points += 2;
-        } else if (isNoNConstSchema(propertySchema) && typeof model[key] !== 'undefined') {
-          points += 1;
-        }
-      });
-      return points;
-    });
-
-    if (schemaPoints.every((p) => p === schemaPoints[0])) {
-      return null;
-    }
-    const bestMatchingSchemaIndex = schemaPoints.indexOf(Math.max(...schemaPoints));
-    return bestMatchingSchemaIndex;
-  };
 
   const getActiveSchemaIndex = (schema, model) => {
     const indexFromModel = getValidIndexFromModel(schema, model);
