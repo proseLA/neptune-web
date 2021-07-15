5c4113080fc054936f55cdd512f80c6308f91ef0
Mikk Narusk <mikk.narusk@transferwise.com>
2021-07-15 15:55:06 +0300

    refactor: rewrite SwitchOption in TS

---
+++ packages/components/src/switchOption/SwitchOption.tsx
@@ -0,0 +1,46 @@
+import React, { ComponentType, MouseEvent, ReactNode } from 'react';
+
+import Option from '../common/Option';
+import Switch from '../switch';
+
+type Props = {
+  checked?: boolean;
+  complex?: boolean;
+  content?: ReactNode;
+  disabled?: boolean;
+  id?: string;
+  media?: ReactNode;
+  onChange: (...args: any[])=>any;
+  title: ReactNode;
+}
+
+
+const SwitchOption: ComponentType<Props> = ({ checked, complex, content, disabled, id, media, onChange, title }) => {
+  const sharedProps = { media, title, content, complex, disabled };
+
+  const stopPropagation = (event: MouseEvent<HTMLSpanElement>) => {
+    if (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      if (event.nativeEvent && event.nativeEvent.stopImmediatePropagation) {
+        event.nativeEvent.stopImmediatePropagation();
+      }
+    }
+  };
+
+  const toggle = (event: MouseEvent<HTMLSpanElement>) => {
+    stopPropagation(event);
+
+    onChange(!checked);
+  };
+
+  return (
+    <Option
+      {...sharedProps}
+      onClick={toggle}
+      button={<Switch id={id} checked={checked} onClick={toggle} />}
+    />
+  );
+};
+
+export default SwitchOption;
