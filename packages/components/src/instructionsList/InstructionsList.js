b2d1a71794ea01aff85aa62831f4f05b4e9e2d9a
joe-sayer <sayerj97@gmail.com>
2021-07-15 11:52:41 +0100

    feat: InstructionsList RTL support

--- packages/components/src/instructionsList/InstructionsList.js
+++ packages/components/src/instructionsList/InstructionsList.js
@@ -4,27 +4,34 @@ import {
   CrossCircle as CrossCircleIcon,
   CheckCircle as CheckCircleIcon,
 } from '@transferwise/icons';
+import classNames from 'classnames';
+
+import { useDirection } from '../common/hooks';
 
 import './InstructionsList.css';
 
-const InstructionsList = ({ dos, donts }) => (
-  <div className="tw-instructions">
-    {dos.map((doThis, index) => (
-      // eslint-disable-next-line react/no-array-index-key
-      <div className="instruction m-t-1" key={index}>
-        <CheckCircleIcon size={24} className="do" />
-        <p className="m-l-2 m-b-0 text-primary">{doThis}</p>
-      </div>
-    ))}
-    {donts.map((dont, index) => (
-      // eslint-disable-next-line react/no-array-index-key
-      <div className="instruction m-t-1" key={index}>
-        <CrossCircleIcon size={24} className="dont" />
-        <p className="m-l-2 m-b-0 text-primary">{dont}</p>
-      </div>
-    ))}
-  </div>
-);
+const InstructionsList = ({ dos, donts }) => {
+  const { isRTL } = useDirection();
+  const listItemClasses = classNames('m-b-0 text-primary', { 'm-l-2': !isRTL, 'm-r-2': isRTL });
+  return (
+    <div className="tw-instructions">
+      {dos.map((doThis, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div className="instruction m-t-1" key={index}>
+          <CheckCircleIcon size={24} className="do" />
+          <p className={listItemClasses}>{doThis}</p>
+        </div>
+      ))}
+      {donts.map((dont, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div className="instruction m-t-1" key={index}>
+          <CrossCircleIcon size={24} className="dont" />
+          <p className={listItemClasses}>{dont}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 InstructionsList.defaultProps = {
   dos: [],
--- packages/components/src/instructionsList/InstructionsList.spec.js
+++ packages/components/src/instructionsList/InstructionsList.spec.js
@@ -3,7 +3,14 @@ import { shallow } from 'enzyme';
 
 import InstructionsList from '.';
 
+import { useDirection } from '../common/hooks';
+
+jest.mock('../common/hooks/useDirection');
+
 describe('InstructionsList', () => {
+  beforeAll(() => {
+    useDirection.mockImplementation(() => ({ direction: 'rtl', isRTL: true }));
+  });
   it('should render dos list only', () => {
     const dos = ['Test this component', 'With multiple dos'];
     const component = shallow(<InstructionsList dos={dos} />);
@@ -26,5 +33,11 @@ describe('InstructionsList', () => {
     expect(component.find('.do')).toHaveLength(1);
     expect(component.find('.dont')).toHaveLength(1);
     expect(component.find('.instruction')).toHaveLength(2);
+  });
+
+  it('should apply correct CSS classes when isRTL is true', () => {
+    const dos = ['Do this'];
+    const component = shallow(<InstructionsList dos={dos} />);
+    expect(component.find('.text-primary').hasClass('m-r-2')).toEqual(true);
   });
 });
