a515b295ee20615be3a8139d00d0f29872c353ce
Julian Garamendy <julian.garamendy@transferwise.com>
2021-08-03 16:51:24 +0100

    Delete commented out tests

--- packages/dynamic-flows/src/formControl/FormControl.spec.js
+++ packages/dynamic-flows/src/formControl/FormControl.spec.js
@@ -40,10 +40,6 @@ describe('FormControl', () => {
   testSimpleControl('number', 'input[type="number"]', 123456, 'Enter your numeric value here');
   testSimpleControl('textarea', 'textarea', 'Lorem ipsum', 'Please enter your feedback');
 
-  //   testTextControlValidation('text', 'input[type="text"]');
-  //   testTextControlValidation('password', 'input[type="password"]');
-  //   testTextControlValidation('textarea', 'textarea');
-
   testCustomControl('select', Select);
   testCustomControl('date', DateInput);
   testCustomControl('tel', PhoneNumberInput);
@@ -51,58 +47,6 @@ describe('FormControl', () => {
   testCustomControl('text', InputWithDisplayFormat);
   testCustomControl('textarea', TextareaWithDisplayFormat);
   testCustomControl('date-lookup', DateLookup);
-
-  // testAutoComplete('text', 'input');
-  // testAutoComplete('number', 'input');
-  // testAutoComplete('password', 'input');
-  // testAutoComplete('textarea', 'textarea');
-
-  //   xdescribe('type: number - validation', () => {
-  //     beforeEach(() => {
-  //       props = {
-  //         type: 'number',
-  //         value: null,
-  //         min: 2,
-  //         max: 5,
-  //         required: true,
-  //       };
-  //       formGroup = shallow(
-  //         <div className="form-group">
-  //           <FormControl {...{ ...defaultProps, ...props }} />
-  //         </div>,
-  //       );
-  //       component = formGroup.find('tw-form-control');
-  //       input = component.find('input');
-  //     });
-
-  //     testRequiredValidation(() => {
-  //       input.value = 4;
-  //       input.simulate('input');
-  //       $timeout.flush();
-  //     }, 4);
-
-  //     testMinMaxValidation(
-  //       () => {
-  //         // setBelowMin
-  //         input.value = 1;
-  //         input.simulate('input');
-  //         $timeout.flush();
-  //       },
-  //       () => {
-  //         // setAboveMax
-  //         input.value = 6;
-  //         input.simulate('input');
-  //         $timeout.flush();
-  //       },
-  //       () => {
-  //         // setValid
-  //         input.value = 4;
-  //         input.simulate('input');
-  //         $timeout.flush();
-  //       },
-  //       4,
-  //     );
-  //   });
 
   describe('type: select', () => {
     let selectElem;
@@ -280,28 +224,6 @@ describe('FormControl', () => {
     }, 1);
   });
 
-  //   xdescribe('type: file - validation', () => {
-  //     beforeEach(() => {
-  //       props.model = null;
-  //       formGroup = compileTemplate(
-  //         "<div class='form-group'> \
-  //             <label class='control-label'></label> \
-  //             <tw-form-control type='file' \
-  //               ng-model='model' \
-  //               ng-focus='onFocus()' \
-  //               ng-blur='onBlur()' \
-  //               ng-change='onChange(value)' \
-  //               required> \
-  //             </tw-form-control> \
-  //           </div>",
-  //       );
-  //       component = formGroup.find('tw-form-control');
-  //     });
-  //     it('should render twUpload', () => {
-  //       expect(component.find('tw-upload')).toBeTruthy();
-  //     });
-  //   });
-
   describe('type: date', () => {
     let dateInput;
 
@@ -330,67 +252,6 @@ describe('FormControl', () => {
     });
   });
 
-  //   xdescribe('type: date validation', () => {
-  //     const dayInput, yearInput;
-  //     beforeEach(() => {
-  //       props.model = null;
-  //       $scope.ngMin = '2015-04-01';
-  //       $scope.ngMax = '2017-03-24';
-  //       formGroup = compileTemplate(
-  //         "<div class='form-group'> \
-  //             <label class='control-label'></label> \
-  //             <tw-form-control type='date' \
-  //               ng-model='model' \
-  //               locale='en-GB' \
-  //               ng-min='ngMin' \
-  //               ng-max='ngMax' \
-  //               tw-minlength='' \
-  //               ng-required='true' \
-  //               ng-focus='onFocus()' \
-  //               ng-blur='onBlur()' \
-  //               ng-change='onChange(value)'> \
-  //             </tw-form-control> \
-  //           </div>",
-  //       );
-  //       component = formGroup.find('tw-form-control');
-  //       dayInput = component.find('.tw-date-day');
-  //       yearInput = component.find('.tw-date-year');
-  //     });
-
-  //     testRequiredValidation(() => {
-  //       // setValid 2016-01-01
-  //       dayInput.value = '01';
-  //       dayInput.simulate('input');
-  //       yearInput.value = '2016';
-  //       yearInput.simulate('input');
-  //     }, '2016-01-01');
-
-  //     testMinMaxValidation(
-  //       () => {
-  //         // setBelowMin 2010-01-01
-  //         dayInput.value = '01';
-  //         dayInput.simulate('input');
-  //         yearInput.value = '2010';
-  //         yearInput.simulate('input');
-  //       },
-  //       () => {
-  //         // setAboveMax 2020-01-01
-  //         dayInput.value = '01';
-  //         dayInput.simulate('input');
-  //         yearInput.value = '2020';
-  //         yearInput.simulate('input');
-  //       },
-  //       () => {
-  //         // setValid 2016-01-01
-  //         dayInput.value = '01';
-  //         dayInput.simulate('input');
-  //         yearInput.value = '2016';
-  //         yearInput.simulate('input');
-  //       },
-  //       '2016-01-01',
-  //     );
-  //   });
-
   describe('type: hidden', () => {
     let input;
 
@@ -405,38 +266,6 @@ describe('FormControl', () => {
       expect(input.prop('type')).toBe('hidden');
     });
   });
-
-  // function testAutoComplete(controlType, controlSelector) {
-  //   let control;
-
-  //   describe(`when autoComplete is enabled for a ${controlType} control`, () => {
-  //     beforeEach(() => {
-  //       props = {
-  //         type: controlType,
-  //         autoComplete: true,
-  //       };
-  //       control = shallow(<FormControl {...{ ...defaultProps, ...props }} />).find(controlSelector);
-  //     });
-
-  //     it('should disable autocomplete', () => {
-  //       expect(control.prop('autoComplete')).toBe('on');
-  //     });
-  //   });
-
-  //   describe(`when autoComplete is not enabled for a ${controlType} control`, () => {
-  //     beforeEach(() => {
-  //       props = {
-  //         type: controlType,
-  //         autoComplete: false,
-  //       };
-  //       control = shallow(<FormControl {...{ ...defaultProps, ...props }} />).find(controlSelector);
-  //     });
-
-  //     it('should enable autocomplete', () => {
-  //       expect(control.prop('autoComplete')).toBe('disabled');
-  //     });
-  //   });
-  // }
 
   function testSimpleControl(controlType, selector, valueToSet, placeholder) {
     let input;
@@ -592,70 +421,6 @@ describe('FormControl', () => {
     });
   }
 
-  //   function testTextControlValidation(controlType, selector) {
-  //     xdescribe('type: ' + controlType + ' - validation', () => {
-  //       beforeEach(() => {
-  //         props.model = '';
-  //         $scope.pattern = '[a-z]+';
-  //         formGroup = compileTemplate(
-  //           "<div class='form-group'> \
-  //               <label class='control-label'></label> \
-  //               <tw-form-control type='" +
-  //             controlType +
-  //             "' \
-  //                 ng-model='model' \
-  //                 ng-minlength='4' \
-  //                 ng-maxlength='6' \
-  //                 ng-pattern='pattern' \
-  //                 ng-required='true'> \
-  //               </tw-form-control> \
-  //             </div>",
-  //         );
-  //         input = formGroup.find(selector);
-  //         component = formGroup.find('tw-form-control');
-  //       });
-
-  //       testRequiredValidation(() => {
-  //         input.value = 'abcd';
-  //         input.simulate('input');
-  //         $timeout.flush();
-  //       }, 'abcd');
-
-  //       testLengthValidation(
-  //         () => {
-  //           input.value = 'abc';
-  //           input.simulate('input');
-  //           $timeout.flush();
-  //         },
-  //         () => {
-  //           input.value = 'abcdefg';
-  //           input.simulate('input');
-  //           $timeout.flush();
-  //         },
-  //         () => {
-  //           input.value = 'abcd';
-  //           input.simulate('input');
-  //           $timeout.flush();
-  //         },
-  //         'abcd',
-  //       );
-
-  //       testPatternValidation(
-  //         () => {
-  //           input.value = '1';
-  //           input.simulate('input');
-  //           $timeout.flush();
-  //         },
-  //         () => {
-  //           input.value = 'abcd';
-  //           input.simulate('input');
-  //           $timeout.flush();
-  //         },
-  //         'abcd',
-  //       );
-  //     });
-  //   }
-
   function testFocusHandler(performFocus) {
     describe('when focused', () => {
       beforeEach(performFocus);
@@ -693,120 +458,4 @@ describe('FormControl', () => {
       });
     });
   }
-
-  // function testRequiredValidation(setValidValue, expectedModel) {
-  //   xdescribe('when required and no value entered', () => {
-  //     it('should set ngModel.$invalid', () => {
-  //       expect(component.classList).toContain('ng-invalid');
-  //       expect(component.classList).toContain('ng-invalid-required');
-  //     });
-  //     it('should not bind to the model', () => {
-  //       expect(props.model).toBeFalsy();
-  //     });
-  //   });
-
-  //   xdescribe('when required and value is entered', () => {
-  //     beforeEach(setValidValue);
-
-  //     it('should set ngModel.$valid', () => {
-  //       expect(component.classList).toContain('ng-valid-required');
-  //     });
-  //     it('should bind to the model', () => {
-  //       expect(props.model).toBe(expectedModel);
-  //     });
-  //   });
-  // }
-
-  //   function testMinMaxValidation(setBelowMin, setAboveMax, setValid, expectedModel) {
-  //     xdescribe('when value is below min', () => {
-  //       beforeEach(setBelowMin);
-  //       it('should set ngModel.$invalid', () => {
-  //         expect(component.classList).toContain('ng-invalid');
-  //         expect(component.classList).toContain('ng-invalid-min');
-  //       });
-  //       it('should not bind to the model', () => {
-  //         expect(props.model).toBeFalsy();
-  //       });
-  //     });
-
-  //     xdescribe('when value is above max', () => {
-  //       beforeEach(setAboveMax);
-  //       it('should set ngModel.$invalid', () => {
-  //         expect(component.classList).toContain('ng-invalid');
-  //         expect(component.classList).toContain('ng-invalid-max');
-  //       });
-  //       it('should not bind to the model', () => {
-  //         expect(props.model).toBeFalsy();
-  //       });
-  //     });
-
-  //     xdescribe('when value is between min and max', () => {
-  //       beforeEach(setValid);
-  //       it('should set ngModel.$valid', () => {
-  //         expect(component.classList).toContain('ng-valid');
-  //         expect(component.classList).toContain('ng-valid-min');
-  //         expect(component.classList).toContain('ng-valid-max');
-  //       });
-  //       it('should bind the value to the model', () => {
-  //         expect(props.model).toBe(expectedModel);
-  //       });
-  //     });
-  //   }
-
-  //   function testLengthValidation(setBelowMin, setAboveMax, setValid, expectedModel) {
-  //     xdescribe('when entered value is shorter than min length', () => {
-  //       beforeEach(setBelowMin);
-  //       it('should set ngModel.$invalid', () => {
-  //         expect(component.classList).toContain('ng-invalid');
-  //         expect(component.classList).toContain('ng-invalid-minlength');
-  //       });
-  //       it('should not bind to the model', () => {
-  //         expect(props.model).toBeFalsy();
-  //       });
-  //     });
-
-  //     xdescribe('when entered value is longer than max length', () => {
-  //       beforeEach(setAboveMax);
-  //       it('should set ngModel.$valid when value is longer than min length', () => {
-  //         expect(component.classList).toContain('ng-invalid');
-  //         expect(component.classList).toContain('ng-invalid-maxlength');
-  //       });
-  //       it('should not bind to the model', () => {
-  //         expect(props.model).toBeFalsy();
-  //       });
-  //     });
-
-  //     xdescribe('when entered value is between min and max length', () => {
-  //       beforeEach(setValid);
-  //       it('should set ngModel.$valid', () => {
-  //         expect(component.classList).toContain('ng-valid-maxlength');
-  //       });
-  //       it('should bind to the model', () => {
-  //         expect(props.model).toBe(expectedModel);
-  //       });
-  //     });
-  //   }
-
-  //   function testPatternValidation(setInvalid, setValid, expectedModel) {
-  //     xdescribe('when entered value does not match pattern', () => {
-  //       beforeEach(setInvalid);
-  //       it('should set ngModel.$invalid', () => {
-  //         expect(component.classList).toContain('ng-invalid');
-  //         expect(component.classList).toContain('ng-invalid-pattern');
-  //       });
-  //       it('should not bind to the model', () => {
-  //         expect(props.model).toBeFalsy();
-  //       });
-  //     });
-
-  //     xdescribe('when entered value matches pattern', () => {
-  //       beforeEach(setValid);
-  //       it('should set ngModel.$valid', () => {
-  //         expect(component.classList).toContain('ng-valid-pattern');
-  //       });
-  //       it('should bind to the model', () => {
-  //         expect(props.model).toBe(expectedModel);
-  //       });
-  //     });
-  //   }
 });
