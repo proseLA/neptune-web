a90a9b832273709d0c8d1107844fea108e4e4abc
CircleCI <circleci@transferwise.com>
2021-08-06 14:11:10 +0000

    Updates

---
+++ branch/DS-1027-remove-deprecated-props/_next/static/chunks/pages/about/Setup-8eb391096d47a17a1233.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[738],{22393:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Setup",function(){return _(55427)}])}},function(n){n.O(0,[9774,2888,179],(function(){return u=22393,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
