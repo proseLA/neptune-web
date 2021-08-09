fe5cfff500a764e573db1e59e3c51b8473a267eb
CircleCI <circleci@transferwise.com>
2021-08-09 08:47:39 +0000

    Updates

---
+++ branch/crowdin_l10n_main/_next/static/chunks/pages/blog/2021-02-01-february-release-40bb734d1b76604d4c11.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1331],{41852:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/2021-02-01-february-release",function(){return u(98217)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=41852,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
