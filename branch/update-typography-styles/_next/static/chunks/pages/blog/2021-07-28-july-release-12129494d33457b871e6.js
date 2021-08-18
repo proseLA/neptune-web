664c1a4100120b7a347f1324b1b1fc27ad057f0b
CircleCI <circleci@transferwise.com>
2021-08-18 11:39:29 +0000

    Updates

---
+++ branch/update-typography-styles/_next/static/chunks/pages/blog/2021-07-28-july-release-12129494d33457b871e6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3572],{43852:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/2021-07-28-july-release",function(){return u(31856)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=43852,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
