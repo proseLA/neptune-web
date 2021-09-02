f71991f80d11e2f1cea05c1132166d768968f4a4
CircleCI <circleci@transferwise.com>
2021-09-02 12:59:47 +0000

    Updates

---
+++ branch/fix-df-storybook/_next/static/chunks/pages/about/Versioning-0dd61dc5346718d850fa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9249],{2388:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/Versioning",function(){return u(59903)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=2388,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
