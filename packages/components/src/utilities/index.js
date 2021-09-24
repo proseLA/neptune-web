f306c7618c43a5f868c63c60c281428cd9a94411
Mate Bek <71433023+matebek-tw@users.noreply.github.com>
2021-09-24 17:12:08 +0200

    feat: force minor version bump in all affected packages (#1160)

--- packages/components/src/utilities/index.js
+++ packages/components/src/utilities/index.js
@@ -1,2 +1,2 @@
-export { logActionRequired, logActionRequiredIf } from './logActionRequired';
 export { default as deprecated } from './deprecatedProperty';
+export { logActionRequired, logActionRequiredIf } from './logActionRequired';
