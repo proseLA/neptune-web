ac5a1234186285569a7b220d6196a4e1db1b8fee
CircleCI <circleci@transferwise.com>
2021-09-28 09:59:58 +0000

    Updates

---
+++ branch/update-typography-styles/_next/static/chunks/pages/styles/tokens/Colors-b583299d9f2e9769207f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4741],{81299:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/styles/tokens/Colors",function(){return u(37221)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=81299,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
