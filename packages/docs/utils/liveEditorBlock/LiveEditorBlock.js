be6cd85e5cd1f8cedc39163456f6c9747f7f7dc8
Naiara Abaroa <naiara.abaroa@transferwise.com>
2021-10-05 10:48:50 +0100

    fix: checkbox CSS specificity (#1175)

--- packages/docs/utils/liveEditorBlock/LiveEditorBlock.js
+++ packages/docs/utils/liveEditorBlock/LiveEditorBlock.js
@@ -4,26 +4,26 @@ import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import theme from '../prismTheme';
 
 const LiveEditorBlock = ({ code, scope, display }) => (
-  <div
-    // react-live is broken at RTL mode
-    // and it doesn't have support for this feature (e.g can't be configured via some prop) either
-    // so enforcing LTR mode in any case
-    dir="ltr"
-    className="row live-provider"
-  >
+  <div className="row live-provider">
     <LiveProvider code={code} scope={scope}>
       {display === 'vertical' ? (
         <>
           <div className="col-xs-12 live-preview m-b-5 p-x-0">
             <LivePreview />
           </div>
-          <div className="col-xs-12 live-editor m-b-5">
+          <div
+            // react-live is broken at RTL mode
+            // and it doesn't have support for this feature (e.g can't be configured via some prop) either
+            // so enforcing LTR mode in any case
+            dir="ltr"
+            className="col-xs-12 live-editor m-b-5"
+          >
             <LiveEditor theme={theme} />
           </div>
         </>
       ) : (
         <>
-          <div className="col-xl-6 live-editor">
+          <div dir="ltr" className="col-xl-6 live-editor">
             <LiveEditor theme={theme} />
           </div>
           <div className="col-xl-6 live-preview">
