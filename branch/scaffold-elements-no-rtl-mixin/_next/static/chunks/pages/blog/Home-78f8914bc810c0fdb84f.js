3bcfcf92eae7fb610ea2414172dcebd39efd2a84
nabaroa <nabaroa@users.noreply.github.com>
2021-10-14 15:15:34 +0000

    Deploying to gh-pages from @ transferwise/neptune-web@0917547aa36cd04b9319580ebb280b60818f3f15 🚀

---
+++ branch/scaffold-elements-no-rtl-mixin/_next/static/chunks/pages/blog/Home-78f8914bc810c0fdb84f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[675],{9503:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/Home",function(){return u(68289)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=9503,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
