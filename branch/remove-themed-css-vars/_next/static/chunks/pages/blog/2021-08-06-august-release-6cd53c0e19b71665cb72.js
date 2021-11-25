6037cf73aa69a76c6f1dcc7b66b7b1ea83f1965d
matebek-tw <matebek-tw@users.noreply.github.com>
2021-11-25 10:58:28 +0000

    Deploying to gh-pages from @ transferwise/neptune-web@c7761612723427f18a97835a066e991bccdbbfd3 🚀

---
+++ branch/remove-themed-css-vars/_next/static/chunks/pages/blog/2021-08-06-august-release-6cd53c0e19b71665cb72.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6330],{40085:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/2021-08-06-august-release",function(){return _(93273)}])}},function(n){n.O(0,[9774,2888,179],(function(){return u=40085,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
