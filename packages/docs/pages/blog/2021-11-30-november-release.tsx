bf62ba247d12d0529338153c9b524001c63986ef
Helen Durrant <helen.durrant@transferwise.com>
2021-11-30 09:06:00 +0000

    docs: updated nov docs & and tokens upgrade guide

--- packages/docs/pages/blog/2021-11-30-november-release.tsx
+++ packages/docs/pages/blog/2021-11-30-november-release.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/function-component-definition */
-import { Alert, Link, Sentiment } from '@transferwise/components';
+import { Alert, Link, Image, Sentiment } from '@transferwise/components';
 import { ReactElement } from 'react';
 
 import Heading from '../../components/Heading';
+import coloursImg from '../../public/static/assets/blog/colours-release-nov-2021.png';
+import rtlImg from '../../public/static/assets/blog/money-rtl-release-nov-2021.png';
 import { DocumentLink } from '../../utils/pageUtils';
 import { Meta } from '../../utils/sections';
 
@@ -28,7 +30,7 @@ export const meta: Meta = {
       githubUsername: 'dozortsev',
     },
   ],
-  tags: ['Typography', 'Accessible Colors', 'RTL'],
+  tags: ['Typography', 'Accessible Colors', 'Right to Left'],
 };
 
 export default function PageContent(): ReactElement {
@@ -44,8 +46,8 @@ export default function PageContent(): ReactElement {
         we did work to address this, and in this release we put our new accessible colours to work.
         We've updated our components, base CSS and docs to use the new colours.
       </p>
-      <div style={{ maxWidth: '300px' }} className="align-self-center m-y-4">
-        {/* TODO: colours image <Image src={popoverNew} alt="New Popover" loading="lazy" stretch={false} /> */}
+      <div style={{ maxWidth: 757 }} className="align-self-center m-y-4">
+        <Image src={coloursImg} alt="New accessible colours" loading="lazy" stretch={false} />
       </div>
       <Heading as="h3" className="m-t-4">
         Semantic colours
@@ -56,8 +58,7 @@ export default function PageContent(): ReactElement {
         were looking to apply some colour to some paragraph text you should use{' '}
         <code>--color-content-primary</code>, rather than <code>@color-base-navy-mid</code>. Learn
         more about{' '}
-        <Link href="https://neptune.wise.com/design/colour/">choosing colours semantically</Link>{' '}
-        over on the main Neptune site.
+        <Link href="https://neptune.wise.com/design/colour/">choosing colours semantically</Link> .
       </p>
       <p>
         You'll notice that the above example makes use of CSS custom properties in place of the old
@@ -73,10 +74,16 @@ export default function PageContent(): ReactElement {
           We strongly recommend using custom properties in place of legacy less variables.
         </strong>{' '}
         This will ease the future work required to support theming, which we're working on at the
-        moment. Note: if you still support IE11, you will need to follow{' '}
-        <DocumentLink href="/about/Styles#tokens-css-custom-properties">this guide</DocumentLink>{' '}
-        before using custom properties.
+        moment.
       </p>
+      <Alert
+        message="If you still support IE 11, you will some extra setup before you can use custom properties."
+        action={{
+          'aria-label': 'Setup custom properties for IE11',
+          href: 'https://transferwise.github.io/neptune-web/about/Styles#installation',
+          text: 'Setup for IE11',
+        }}
+      />
       <p>
         We have updated the name of some of our tokens to better communicate their purpose. See the
         breaking changes section below.
@@ -99,6 +106,43 @@ export default function PageContent(): ReactElement {
         New title classes (<code>.title-1</code>, <code>.title-2</code> etc) are available for
         styling headings.
       </p>
+      <table className="table docs-table">
+        <thead>
+          <tr>
+            <th>Usage</th>
+            <th>Demo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>{'<h1 class="title-1">Money without borders</h1>'}</pre>
+              <pre>{'<span class="title-1">Money without borders</span>'}</pre>
+            </td>
+            <td>
+              <h1 className="title-1">Money without borders</h1>
+              <span className="title-1">Money without borders</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>{'<h2 class="title-2">Money without borders</h2>'}</pre>
+              <pre>{'<span class="title-2">Money without borders</span>'}</pre>
+            </td>
+            <td>
+              <h1 className="title-2">Money without borders</h1>
+              <span className="title-2">Money without borders</span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="text-xs-center body-1">
+              See the{' '}
+              <DocumentLink href="styles/core/Typography#headings">documentation</DocumentLink> for
+              more examples.
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <Heading as="h3" className="m-t-4">
         General text utilities
       </Heading>
@@ -107,31 +151,91 @@ export default function PageContent(): ReactElement {
         Several new utility classes (<code>.body-*</code>, <code>.control-*</code>,{' '}
         <code>.value</code>, <code>.label</code> etc) are available for styling text.
       </p>
+      <table className="table docs-table">
+        <thead>
+          <tr>
+            <th>Usage example</th>
+            <th>Demo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>{`<p class="body-1">Body 1 - money without borders</p>`}</pre>
+            </td>
+            <td>
+              <p className="body-1">Body 1 - money without borders</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>{`<p class="control-1">Control 1</p>`}</pre>
+            </td>
+            <td>
+              <p className="control-1">Control 1</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>{'<p class="label">Label</p>'}</pre>
+            </td>
+            <td>
+              <p className="label">Label</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>{'<p class="value">Value</p>'}</pre>
+            </td>
+            <td>
+              <p className="value">Value</p>
+            </td>
+          </tr>
+
+          <tr>
+            <td colSpan={2} className="text-xs-center body-1">
+              See the{' '}
+              <DocumentLink href="styles/core/Typography#body-copy">documentation</DocumentLink> for
+              more examples.
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <Heading as="h3" className="m-t-4">
         Typography tokens
       </Heading>
-      <DocumentLink href="styles/tokens/Typography">Typography tokens docs</DocumentLink>.
+      <DocumentLink href="styles/tokens/Typography">Typography tokens docs</DocumentLink>
       <p className="m-t-2">
-        We've exposed a set of tokens for font size, line height and font weight.
+        We've exposed a set of tokens for font size, line height and font weight. See the{' '}
+        <DocumentLink href="styles/tokens/Typography">documentation</DocumentLink> for the full
+        list.
       </p>
       <Heading as="h2" className="m-t-4">
         Right to Left
       </Heading>
-      <DocumentLink href="/styles/addons/RtlLayout">RTL setup guide</DocumentLink>
+      <DocumentLink href="/styles/addons/RtlLayout">Setup guide</DocumentLink>
       <p className="m-t-3">
         We have added right to left support for Neptune CSS and all components. If you want to get
         started supporting right to left in your application today, we have{' '}
         <DocumentLink href="/styles/addons/RtlLayout">a guide</DocumentLink> describing the steps
         you need to take.
       </p>
+      <div style={{ maxWidth: 837 }} className="align-self-center m-y-4">
+        <Image src={rtlImg} alt="Right to left money input" loading="lazy" stretch={false} />
+      </div>
       <Heading as="h2" className="m-t-4">
         Breaking changes and deprecations
       </Heading>
       <Heading as="h3" className="m-t-4">
-        Token names
+        Tokens changes
       </Heading>
       <Alert className="m-y-2" message="Breaking change" type={Sentiment.NEGATIVE} />
-      <p>Some token names have changed and some have been removed.</p>
+      <p>
+        Some token names have changed and some have been removed. This affects both less variables
+        and CSS custom properties. Please refer to the{' '}
+        <DocumentLink href="/blog/2021-11-30-tokens-upgrade-guide">upgrade guide</DocumentLink> for
+        a breakdown of the changes and instructions on how to upgrade.
+      </p>
       <Heading as="h3" className="m-t-4">
         Typography utility classes deprecated
       </Heading>
---
+++ packages/docs/pages/blog/2021-11-30-tokens-upgrade-guide.tsx
@@ -0,0 +1,238 @@
+import { Alert, Sentiment, Link } from '@transferwise/components';
+import { ReactElement } from 'react';
+
+import Heading from '../../components/Heading';
+import { DocumentLink } from '../../utils/pageUtils';
+
+export const meta = {
+  name: 'Tokens upgrade guide',
+  date: '2021-11-30',
+  hidden: true,
+};
+
+const PageContent = (): ReactElement => {
+  return (
+    <div className="d-flex flex-column">
+      <p>
+        Version <strong>12</strong> of Neptune CSS bumps{' '}
+        <a href="https://github.com/transferwise/neptune-tokens">neptune-tokens</a> from v1 to v6,
+        so there are lots of great updates and some breaking changes that we need to address.{' '}
+        <strong>There is a script you can run to make most of these changes automatically.</strong>
+      </p>
+      <Link href="#steps-to-upgrade">Skip to the upgrade steps</Link>
+      <Heading as="h2">Changes</Heading>
+      <p>
+        Currently, all our tokens are built into both less variables (e.g.{' '}
+        <code>@color-content-primary</code>) and CSS custom properties (
+        <code>--color-content-primary</code>). Although the examples below are written as custom
+        properties, <strong>the changes below apply to both less and custom properties.</strong>
+      </p>
+
+      <Alert
+        className="m-y-2"
+        type="success"
+        message="Prefer CSS custom properties over less variables."
+      />
+
+      <Heading as="h3" className="m-t-4 m-b-1">
+        New tokens
+      </Heading>
+      <table className="table docs-table">
+        <thead>
+          <tr>
+            <th>New token</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>{`--color-content-positive-hover
+--color-content-positive-active
+--color-content-warning-hover
+--color-content-warning-active
+--color-content-negative-hover
+--color-content-negative-active`}</pre>
+            </td>
+            <td>
+              We've added missing combinations of tokens so that we have default, hover and active
+              states for postitive, negative and warning content colours.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>{`--color-content-secondary
+--color-content-secondary-active
+--color-content-secondary-hover`}</pre>
+            </td>
+            <td>We added a secondary content colour variation.</td>
+          </tr>
+          <tr>
+            <td>
+              <pre>--color-interactive-disabled</pre>
+            </td>
+            <td>We added a disabled colour for interactive elements.</td>
+          </tr>
+          <tr>
+            <td>
+              <pre>{`--color-border-overlay
+--color-background-overlay`}</pre>
+            </td>
+            <td>We added overlay colours.</td>
+          </tr>
+          <tr>
+            <td>
+              <pre>--color-core-contrast</pre>
+            </td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>
+              <pre>--space-content-horizontal</pre>
+            </td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>
+              <pre>--font-family-regular</pre>
+            </td>
+            <td>
+              While you shouldn’t need to set the font often, on the occasions that you do, you can
+              now use <code>font-family-regular</code>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <Heading as="h3" className="m-t-4 m-b-1">
+        Updated tokens
+      </Heading>
+      <Alert className="m-y-2" type={Sentiment.NEGATIVE} message="Breaking change" />
+      <table className="table docs-table">
+        <thead>
+          <tr>
+            <th>Old token</th>
+            <th>New token</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>--color-text-*</pre>
+            </td>
+            <td>
+              <pre>--color-content-*</pre>
+            </td>
+            <td>
+              All <code>-text</code> tokens are now <code>-content</code> to better reflect that
+              they should be used for more than just text (icons, for example).
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>--color-control-*</pre>
+            </td>
+            <td>
+              <pre>--color-interactive-*</pre>
+            </td>
+            <td>
+              All <code>-control</code> tokens are now <code>-interactive</code> to emphasise that
+              these tokens should be used to colour interactive elements.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <Heading as="h3" className="m-t-4 m-b-1">
+        Removed tokens
+      </Heading>
+      <Alert className="m-y-2" type={Sentiment.NEGATIVE} message="Breaking change" />
+
+      <table className="table docs-table">
+        <thead>
+          <tr>
+            <th>Old token</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>--color-text-control</pre>
+            </td>
+            <td>
+              Should be replaced with <code>#fff</code>, as this token is not themed.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>--color-text-important</pre>
+            </td>
+            <td>
+              Should be replaced with <code>--color-content-primary</code>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <pre>{`--color-border-focus
+--color-border-positive
+--color-border-negative
+--color-border-warning`}</pre>
+            </td>
+            <td>
+              Most people won't need this, but if you find you're using this, try{' '}
+              <code>--color-interactive-accent</code> instead
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <Heading as="h3" className="m-t-4 m-b-1">
+        Deprecated tokens
+      </Heading>
+      <Alert
+        className="m-y-2"
+        type={Sentiment.WARNING}
+        message="These tokens are considered legacy and may be removed in an upcoming release."
+      />
+
+      <table className="table docs-table">
+        <thead>
+          <tr>
+            <th>Deprecated token</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>{`--color-primary
+--color-secondary
+--color-accent
+--color-positive
+--color-negative
+--color-warning`}</pre>
+            </td>
+            <td>
+              These are tricky to replace using a find and replace - you need to look at the usage
+              and decide whether a <code>--color-content-</code> or{' '}
+              <code>--color-interactive-</code> colour is best, depending on where it's being used.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Heading as="h2" className="m-t-4 m-b-1">
+        Steps to upgrade
+      </Heading>
+      <Heading as="h3" className="m-t-4 m-b-1">
+        1: Run this script
+      </Heading>
+
+      <Heading as="h2">Dependency requirements</Heading>
+      <pre>{'@transferwise/neptune-css >= 12.x.x'}</pre>
+    </div>
+  );
+};
+
+export default PageContent;
