e0ba2d4344a0367ee88ef2cfa60b8b2d851928b6
helendurranttw <helendurranttw@users.noreply.github.com>
2021-12-01 17:10:26 +0000

    Deploying to gh-pages from @ transferwise/neptune-web@f4084ec2f8ba86e43eee7ba32de2e4e82882efe0 🚀

---
+++ branch/release-docs-code-mod/_next/static/chunks/pages/blog/2021-11-30-typography-upgrade-guide-d213bf24325cce1c13df.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7773],{41472:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/2021-11-30-typography-upgrade-guide",function(){return _(72002)}])}},function(n){n.O(0,[9774,2888,179],(function(){return u=41472,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
