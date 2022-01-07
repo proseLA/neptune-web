c534e4ed34c4fe58438009ae6e238f2ba1e537ca
nabaroa <nabaroa@users.noreply.github.com>
2022-01-07 12:17:49 +0000

    Deploying to gh-pages from @ transferwise/neptune-web@0f3928eeabc34ecf28e4e460140210346eb75be3 🚀

---
+++ branch/select/_next/static/chunks/pages/blog-53134b7e75b95bf2e5a6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9195],{42047:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog",function(){return u(7434)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=42047,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
