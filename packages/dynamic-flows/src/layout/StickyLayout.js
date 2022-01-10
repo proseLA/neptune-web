1c8e635244202bdd3de71b3b911f747aa0ea2554
richard.bata <richard.bata@transferwise.com>
2022-01-10 08:38:49 -0800

    Cleanup

--- packages/dynamic-flows/src/layout/StickyLayout.js
+++ packages/dynamic-flows/src/layout/StickyLayout.js
@@ -3,24 +3,6 @@ import Types from 'prop-types';
 const StickyLayout = (props) => {
   const { children, stickyContent } = props;
 
-  // const isStickyButton = true;
-  //
-  // if (isStickyButton) {
-  //   component.action.margin = 'xs';
-  //
-  //   return (
-  //     <>
-  //       <div className="visible-xs-block visible-sm-block">
-  //         <Sticky>
-  //           <div className="p-a-2">{getButton(component)}</div>
-  //         </Sticky>
-  //       </div>
-  //
-  //       <div className="hidden-xs hidden-sm">{getButton(component)}</div>
-  //     </>
-  //   );
-  // }
-
   return (
     <div className="sticky-container">
       <div className="sticky-content">
