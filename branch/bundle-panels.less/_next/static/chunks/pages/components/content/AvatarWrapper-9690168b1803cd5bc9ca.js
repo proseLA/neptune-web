6b5cd74edaf71e048e457970f21ceeb16b6b56ac
dozortsev <dozortsev@users.noreply.github.com>
2022-01-14 12:08:45 +0000

    Deploying to gh-pages from @ transferwise/neptune-web@27fcdf1950f825020bba539bcf5a9ba2d565adee 🚀

---
+++ branch/bundle-panels.less/_next/static/chunks/pages/components/content/AvatarWrapper-9690168b1803cd5bc9ca.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1589],{40629:function(n,_,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/content/AvatarWrapper",function(){return t(17800)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=40629,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
