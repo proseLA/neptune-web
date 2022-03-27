85612204b083b4e089575206c9f480afb45fb9ef
JulianG-TW <JulianG-TW@users.noreply.github.com>
2022-03-27 21:03:02 +0000

    Deploying to gh-pages from @ transferwise/neptune-web@de28d590a525f2c9a33970c94bb113d861f00557 🚀

---
+++ branch/cleanup-translations-prop/_next/static/chunks/pages/components/internal/Sticky-ae86c7a569832ffe7e53.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4970],{87541:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/internal/Sticky",function(){return u(43030)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=87541,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
