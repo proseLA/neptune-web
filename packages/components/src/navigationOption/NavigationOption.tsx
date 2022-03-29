969ca0ef709e12c948373db8ec126db6f1aac7dd
striccaa <22946856+striccaa@users.noreply.github.com>
2022-03-29 10:25:55 +0200

    feat: update NavigationOption with TS types and forwardRef wrapper

--- packages/components/src/navigationOption/NavigationOption.tsx
+++ packages/components/src/navigationOption/NavigationOption.tsx
@@ -1,91 +1,40 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import requiredIf from 'react-required-if';
+import { forwardRef } from 'react';
 
 import Chevron from '../chevron';
 import { Position } from '../common';
 import Option from '../common/Option';
+import { OptionProps } from '../common/Option/Option';
 
-const NavigationOption = ({
-  'aria-label': ariaLabel,
-  as,
-  media,
-  href,
-  target,
-  title,
-  content,
-  onClick,
-  complex,
-  disabled,
-  showMediaAtAllSizes,
-  showMediaCircle,
-  className,
-}) => {
-  const sharedProps = {
-    ariaLabel,
-    as,
-    media,
-    href,
-    target,
-    title,
-    content,
-    complex,
-    disabled,
-    showMediaAtAllSizes,
-    showMediaCircle,
-    className: classNames('np-navigation-option', className),
-  };
+type NavigationOptionProps = OptionProps;
 
-  if (href) {
-    sharedProps.as = 'a';
-  }
+const NavigationOption = forwardRef<HTMLElement, NavigationOptionProps>(
+  ({ as: component = 'button', disabled = false, onClick, className, ...rest }, reference) => {
+    if ('href' in rest) {
+      component = 'a';
+    }
 
-  return (
-    <Option
-      {...sharedProps}
-      button={<Chevron orientation={Position.RIGHT} disabled={disabled} className="d-block" />}
-      onClick={(event) => {
-        if (disabled) {
-          event.preventDefault();
-          return;
-        }
+    return (
+      <Option
+        {...rest}
+        ref={reference}
+        as={component}
+        className={classNames('np-navigation-option', className)}
+        button={<Chevron orientation={Position.RIGHT} disabled={disabled} className="d-block" />}
+        disabled={disabled}
+        onClick={(event) => {
+          if (disabled) {
+            event.preventDefault();
+            return;
+          }
 
-        onClick(event);
-      }}
-    />
-  );
-};
-
-NavigationOption.propTypes = {
-  'aria-label': PropTypes.string,
-  as: PropTypes.string,
-  media: PropTypes.node,
-  title: PropTypes.node.isRequired,
-  content: PropTypes.node,
-  onClick: PropTypes.func,
-  href: requiredIf(PropTypes.string, (props) => props.as === 'a'),
-  target: PropTypes.oneOf(['_self', '_blank', '_parent', '_top']),
-  complex: PropTypes.bool,
-  disabled: PropTypes.bool,
-  showMediaAtAllSizes: PropTypes.bool,
-  showMediaCircle: PropTypes.bool,
-  className: PropTypes.string,
-};
-
-NavigationOption.defaultProps = {
-  'aria-label': undefined,
-  as: 'button',
-  media: null,
-  content: null,
-  complex: false,
-  onClick: null,
-  href: undefined,
-  target: undefined,
-  disabled: false,
-  showMediaAtAllSizes: false,
-  showMediaCircle: true,
-  className: null,
-};
+          if (onClick) {
+            onClick(event);
+          }
+        }}
+      />
+    );
+  },
+);
 
 export default NavigationOption;
