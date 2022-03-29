96c0ff49e53187aa8a66fc90f14945a47a85f8ab
striccaa <22946856+striccaa@users.noreply.github.com>
2022-03-29 10:24:47 +0200

    feat: update Option component with TS types and forwardRef wrapper

--- packages/components/src/common/Option/Option.tsx
+++ packages/components/src/common/Option/Option.tsx
@@ -1,102 +1,90 @@
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import requiredIf from 'react-required-if';
+import { MouseEvent, forwardRef, HTMLAttributes, ReactNode, ElementType } from 'react';
 
-const Option = ({
-  'aria-label': ariaLabel,
-  as: Element,
-  href,
-  target,
-  media,
-  title,
-  content,
-  onClick,
-  htmlFor,
-  disabled,
-  button,
-  decision,
-  complex,
-  className,
-  inverseMediaCircle,
-  showMediaAtAllSizes,
-  showMediaCircle,
-}) => (
-  <Element
-    className={classNames('np-option', className, {
-      'decision-complex': complex,
-      decision,
-      disabled,
-      'np-option__sm-media': showMediaAtAllSizes,
-    })}
-    href={href}
-    htmlFor={htmlFor}
-    target={target}
-    disabled={disabled && Element === 'button'}
-    aria-label={ariaLabel}
-    onClick={onClick}
-  >
-    <div className="media">
-      {media && (
-        <div className="media-left">
-          {showMediaCircle ? (
-            <div
-              className={classNames('circle circle-sm text-primary', {
-                'circle-inverse': inverseMediaCircle,
-              })}
-            >
-              {media}
+interface BaseOptionProps extends Omit<HTMLAttributes<HTMLElement>, 'title'> {
+  'aria-label'?: string;
+  media?: ReactNode;
+  htmlFor?: string;
+  title: ReactNode;
+  content?: ReactNode;
+  onClick?: (event: MouseEvent<HTMLElement>) => void;
+  as?: ElementType;
+  disabled?: boolean;
+  button?: ReactNode;
+  decision?: boolean;
+  complex?: boolean;
+  inverseMediaCircle?: boolean;
+  className?: string;
+  showMediaAtAllSizes?: boolean;
+  showMediaCircle?: boolean;
+}
+
+interface AnchorOptionProps extends BaseOptionProps {
+  as: 'a';
+  href: string;
+  target: '_self' | '_blank' | '_parent' | '_top';
+}
+
+export type OptionProps = BaseOptionProps | AnchorOptionProps;
+
+const Option = forwardRef<HTMLElement, OptionProps>(
+  (
+    {
+      media = '',
+      title,
+      content,
+      as: component,
+      button,
+      decision = true,
+      disabled = false,
+      complex,
+      inverseMediaCircle = true,
+      className,
+      showMediaAtAllSizes,
+      showMediaCircle = true,
+      ...rest
+    },
+    reference,
+  ) => {
+    const Element = (component as ElementType) ?? 'label';
+
+    return (
+      <Element
+        {...rest}
+        ref={reference}
+        className={classNames('np-option', className, {
+          'decision-complex': complex,
+          decision,
+          disabled,
+          'np-option__sm-media': showMediaAtAllSizes,
+        })}
+        disabled={disabled && Element === 'button'}
+      >
+        <div className="media">
+          {media && (
+            <div className="media-left">
+              {showMediaCircle ? (
+                <div
+                  className={classNames('circle circle-sm text-primary', {
+                    'circle-inverse': inverseMediaCircle,
+                  })}
+                >
+                  {media}
+                </div>
+              ) : (
+                <div className="np-option__no-media-circle">{media}</div>
+              )}
             </div>
-          ) : (
-            <div className="np-option__no-media-circle">{media}</div>
           )}
+          <div className="media-body">
+            <div className="decision__title">{title}</div>
+            {content && <div className="decision__content">{content}</div>}
+          </div>
+          <div className="media-right">{button}</div>
         </div>
-      )}
-      <div className="media-body">
-        <div className="decision__title">{title}</div>
-        {content && <div className="decision__content">{content}</div>}
-      </div>
-      <div className="media-right">{button}</div>
-    </div>
-  </Element>
+      </Element>
+    );
+  },
 );
-
-Option.propTypes = {
-  'aria-label': PropTypes.string,
-  media: PropTypes.node,
-  htmlFor: PropTypes.string,
-  title: PropTypes.node.isRequired,
-  content: PropTypes.node,
-  onClick: PropTypes.func,
-  href: requiredIf(PropTypes.string, (props) => props.as === 'a'),
-  target: PropTypes.oneOf(['_self', '_blank', '_parent', '_top']),
-  as: PropTypes.string,
-  disabled: PropTypes.bool,
-  button: PropTypes.node,
-  decision: PropTypes.bool,
-  complex: PropTypes.bool,
-  inverseMediaCircle: PropTypes.bool,
-  className: PropTypes.string,
-  showMediaAtAllSizes: PropTypes.bool,
-  showMediaCircle: PropTypes.bool,
-};
-
-Option.defaultProps = {
-  'aria-label': undefined,
-  as: 'label',
-  media: '',
-  content: null,
-  htmlFor: null,
-  disabled: false,
-  button: null,
-  onClick: null,
-  href: null,
-  target: undefined,
-  decision: true,
-  complex: false,
-  inverseMediaCircle: true,
-  className: null,
-  showMediaAtAllSizes: false,
-  showMediaCircle: true,
-};
 
 export default Option;
