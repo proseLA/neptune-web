8b975752040dc74d8117fb6ba11cde5abcf5c806
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2022-03-31 09:59:35 +0100

    Update ProgressBar.tsx

--- packages/components/src/progressBar/ProgressBar.tsx
+++ packages/components/src/progressBar/ProgressBar.tsx
@@ -20,15 +20,13 @@ const ProgressBar = ({
     <div className={classNames('np-progress-indicator', className)}>
       <div className="np-progress-indicator__title h4">{title}</div>
       {description && <div className="small">{description}</div>}
-      <div className={'np-progress-indicator__bar m-t-1'}>
+      <div className="np-progress-indicator__bar m-t-1">
         <div
           style={{ width: progress }}
-          className={
-            'np-progress-indicator__bar__content np-progress-indicator__bar__content--accent p-y-1'
-          }
+          className="np-progress-indicator__bar__content np-progress-indicator__bar__content--accent p-y-1"
         />
       </div>
-      <div className={'np-progress-indicator__value d-flex justify-content-end h4'}>{value}</div>
+      <div className="np-progress-indicator__value d-flex justify-content-end h4">{value}</div>
     </div>
   );
 };
